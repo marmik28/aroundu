2f5986ab15d2c7a9a9161a451ddf8a3b718db05f
Marmik patel <62463824+marmik28@users.noreply.github.com>
2024-11-26 17:20:42 -0500

    debugging

--- app/page.tsx
+++ app/page.tsx
@@ -14,12 +14,14 @@ export default function Home() {
   const backend_url = process.env.NEXT_PUBLIC_BACKEND
   useEffect(() => {
     const fetchEvents = async () => {
+      console.log("ENV: ", backend_url)
       try {
         const response = await axios.get(`${backend_url}`);
         setEvents(response.data);
       } catch (error) {
         console.error('Error fetching events:', error);
       }
+      console.log("data: ", events)
     };
 
     fetchEvents();
